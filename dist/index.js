27e982f0c244cd4833638ebe6d45f87f59f971d9
Pakho Leung <pakholeung37@gmail.com>
2024-07-12 10:29:10 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -3,20 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-/**
- * trantor console page saver, this script will open all page of the given console url and open the page, click the 'save' button and re-save the page.
- * 1. create a cluster of workers
- * 2. open the console page
- * 3. get all the scene page through api call
- * 4. add all the scene page url to the queue
- * 5. workers will pick the scene page from the queue and open the page and save the page
- * 6. workers will open the page and wait for the page to load. as soon as the save button is enabled, click the save button and re-save the page.
- */
 const puppeteer_1 = __importDefault(require("puppeteer"));
 const bluebird_1 = __importDefault(require("bluebird"));
 const axios_1 = __importDefault(require("axios"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const process_1 = require("process");
+const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config();
 // read arg from command line like `node dist/index.js --parallel 4`
 const args = process.argv.slice(2);
@@ -36,14 +28,10 @@ if (process.env.TEAM_ID === undefined) {
     console.error("Please provide the team id");
     process.exit(1);
 }
-if (process.env.BRANCH_ID === undefined) {
-    console.error("Please provide the branch id");
-    process.exit(1);
-}
-if (process.env.PORTAL_ID === undefined) {
-    console.error("Please provide the portal id");
-    process.exit(1);
-}
+// if (process.env.BRANCH_ID === undefined) {
+//   console.error("Please provide the branch id");
+//   process.exit(1);
+// }
 // default 4
 const parallel = args.includes("--parallel")
     ? Number(args[args.indexOf("--parallel") + 1])
@@ -52,6 +40,15 @@ if (!url) {
     console.error("Please provide the console url");
     process.exit(1);
 }
+const from = args.includes("--from")
+    ? args[args.indexOf("--from") + 1]
+    : "menu";
+const timeoutPerScene = args.includes("--timeout")
+    ? Number(args[args.indexOf("--timeout") + 1])
+    : 10 * 1000;
+const headless = args.includes("--headless")
+    ? args[args.indexOf("--headless") + 1] === "true"
+    : true;
 const request = axios_1.default.create({
     baseURL: url,
     headers: {
@@ -61,23 +58,7 @@ const request = axios_1.default.create({
     },
 });
 const queue = [];
-(async () => {
-    // get all the scene page through api call
-    const menu = await getMenu();
-    console.log("menu: ", menu.length);
-    pushAllScenesFromMenu(menu);
-    await replaceSceneKey(queue);
-    console.log("all scenes: ", queue.length);
-    const browser = await puppeteer_1.default.launch({
-        headless: true,
-        defaultViewport: null,
-        args: ["--start-maximized"],
-    });
-    await bluebird_1.default.map(queue, (scene, index) => processQueue(scene, index, browser), {
-        concurrency: parallel,
-    });
-    (0, process_1.exit)();
-})();
+const errorScenes = [];
 async function getMenu() {
     const menu = await request.get(`/api/trantor/menu/${process.env.PORTAL_ID}`, {
         params: {
@@ -92,21 +73,80 @@ async function getMenu() {
     });
     return menu.data.data;
 }
-function pushAllScenesFromMenu(menu) {
-    for (const item of menu) {
-        if (item.routeType === "Scene") {
-            queue.push({
-                teamId: process.env.TEAM_ID,
-                // TODO wrong appId
-                appId: undefined,
-                branchId: process.env.BRANCH_ID,
-                key: item.routeConfig.sceneKey,
+/**
+ * 从菜单中获取所有的场景
+ * @param menu
+ */
+async function pushAllScenesFromMenu() {
+    const pushAllScenesFromMenuInternal = (menu) => {
+        for (const item of menu) {
+            if (item.routeType === "Scene") {
+                queue.push({
+                    teamId: process.env.TEAM_ID,
+                    // TODO wrong appId
+                    appId: undefined,
+                    key: item.routeConfig.sceneKey,
+                });
+            }
+            if (item.children?.length) {
+                pushAllScenesFromMenuInternal(item.children);
+            }
+        }
+    };
+    const menu = await getMenu();
+    pushAllScenesFromMenuInternal(menu);
+    console.log("menu: ", menu.length);
+}
+/**
+ *
+ */
+async function getModules(teamId) {
+    const modules = await request.get(`/api/trantor/console/module/query?type=Module`, {
+        headers: {
+            "Trantor2-Team": process.env.TEAM_ID,
+            "Trantor2-Branch": process.env.BRANCH_ID,
+        },
+    });
+    return modules.data.data;
+}
+async function getScenesFromModule(teamId, moduleId) {
+    const { data: resourcesTree } = await request.get(`/api/trantor/console/meta-data/resource-tree/folders?teamId=${teamId}&appId=${moduleId}&v2=true`, {
+        headers: {
+            "Trantor2-App": moduleId,
+            "Trantor2-Team": process.env.TEAM_ID,
+            "Trantor2-Branch": process.env.BRANCH_ID,
+        },
+    });
+    // filter all the scenes
+    const scenes = [];
+    const findScenes = (resource) => {
+        if (resource.type === "Scene") {
+            scenes.push({
+                key: resource.key,
+                appId: moduleId,
+                teamId: teamId,
+                // branchId: process.env.BRANCH_ID!,
             });
         }
-        if (item.children?.length) {
-            pushAllScenesFromMenu(item.children);
+        if (resource.children) {
+            resource.children.forEach(findScenes);
         }
-    }
+    };
+    resourcesTree.data.forEach(findScenes);
+    return scenes;
+}
+/**
+ * 从模块中获取所有的场景
+ * @param modules
+ */
+async function pushAllScenesFromModules() {
+    const modules = await getModules(process.env.TEAM_ID);
+    const scenes = await Promise.all(modules.map((module) => getScenesFromModule(process.env.TEAM_ID, module.id)));
+    scenes.forEach((scene) => queue.push(...scene));
+}
+async function pushAllScenesFromModule(moduleId) {
+    const scenes = await getScenesFromModule(process.env.TEAM_ID, moduleId);
+    queue.push(...scenes);
 }
 async function replaceSceneKey(scenes) {
     const allSceneKeys = queue.map((scene) => scene.key);
@@ -132,7 +172,7 @@ async function replaceSceneKey(scenes) {
     });
 }
 async function processQueue(sceneMeta, index, browser) {
-    const retries = 3;
+    const retries = 2;
     for (let i = 0; i < retries; i++) {
         const page = await browser.newPage();
         // set cookie
@@ -142,23 +182,23 @@ async function processQueue(sceneMeta, index, browser) {
             domain: process.env.COOKIE_DOMAIN,
         });
         // validate the sceneMeta
-        if (!sceneMeta.appId || !sceneMeta.branchId || !sceneMeta.key) {
+        if (!sceneMeta.appId || !sceneMeta.key) {
             console.error("invalid sceneMeta", sceneMeta);
             return;
         }
-        const sceneUrl = `${url}/team/${sceneMeta.teamId}/branch/${sceneMeta.branchId}/app/${sceneMeta.appId}/scene/${sceneMeta.key}`;
+        const sceneUrl = `${url}/team/${sceneMeta.teamId}${process.env.BRANCH_ID ? `/branch/${process.env.BRANCH_ID}` : ""}/app/${sceneMeta.appId}/scene/${sceneMeta.key}`;
         console.log("opening scene", sceneUrl);
         await page.goto(sceneUrl);
         try {
-            // 30s timeout, if the page is not loaded in 30s, it will throw error
-            const timeout = 10 * 1000;
             // wait for id=scene-save-button to be enabled
-            await page.waitForSelector("#scene-save-button:enabled", { timeout });
+            await page.waitForSelector("#scene-save-button:enabled", {
+                timeout: timeoutPerScene,
+            });
             // click save button
             await page.click("#scene-save-button");
             // wait for button to be not .ant-btn-loading
             await page.waitForSelector("#scene-unlock-button", {
-                timeout,
+                timeout: timeoutPerScene,
             });
             console.log("scene successfully saved", sceneUrl);
             console.log("scenes index: ", index);
@@ -167,8 +207,8 @@ async function processQueue(sceneMeta, index, browser) {
             await request.post(`/api/trantor/console/dlock/unlock/${sceneMeta.key}`, {}, {
                 headers: {
                     "Trantor2-App": sceneMeta.appId,
-                    "Trantor2-Team": sceneMeta.teamId,
-                    "Trantor2-Branch": sceneMeta.branchId,
+                    "Trantor2-Team": process.env.TEAM_ID,
+                    "Trantor2-Branch": process.env.BRANCH_ID,
                 },
             });
             console.log("scene unlocked", sceneMeta.key);
@@ -176,9 +216,73 @@ async function processQueue(sceneMeta, index, browser) {
         }
         catch (e) {
             console.error(e, sceneMeta.key, sceneMeta);
+            if (retries === i + 1) {
+                errorScenes.push(sceneMeta);
+            }
             await page.close();
         }
         console.log("retrying scene", sceneMeta.key);
     }
 }
+(async function main() {
+    // get all the scene page through api call
+    if (from === "modules") {
+        await pushAllScenesFromModules();
+    }
+    else if (from === "module") {
+        if (process.env.APP_ID === undefined) {
+            console.error("Please provide the portal id");
+            process.exit(1);
+        }
+        await pushAllScenesFromModule(process.env.APP_ID);
+    }
+    else {
+        if (process.env.PORTAL_ID === undefined) {
+            console.error("Please provide the portal id");
+            process.exit(1);
+        }
+        await pushAllScenesFromMenu();
+        await replaceSceneKey(queue);
+    }
+    console.log("all scenes: ", queue.length);
+    const browser = await puppeteer_1.default.launch({
+        headless,
+        defaultViewport: null,
+        args: ["--start-maximized"],
+    });
+    await bluebird_1.default.map(queue, (scene, index) => processQueue(scene, index, browser), {
+        concurrency: parallel,
+    });
+    console.log("error scenes", errorScenes);
+    console.log("error scenes count", errorScenes.length);
+    await browser.close();
+    if (errorScenes.length === 0) {
+        console.log("All scenes are successfully saved");
+        (0, process_1.exit)();
+    }
+    // write error scenes to log file
+    const currentDate = new Date();
+    const host = process.env.HOST || "localhost";
+    const subdomain = host.split(".")[0];
+    const name = subdomain.split("://")[1];
+    const teamId = process.env.TEAM_ID || "unknown";
+    const appId = process.env.APP_ID || "unknown";
+    const portalId = process.env.PORTAL_ID || "unknown";
+    let fileName = `${name}-${teamId}`;
+    if (from === "menu") {
+        fileName += `-portal${portalId}`;
+    }
+    else if (from === "module") {
+        fileName += `-module${appId}`;
+    }
+    else {
+    }
+    fileName += `-${currentDate.toISOString()}.log`;
+    const logDirectory = "logs";
+    if (!fs_1.default.existsSync(logDirectory)) {
+        fs_1.default.mkdirSync(logDirectory);
+    }
+    fs_1.default.writeFileSync(`${logDirectory}/${fileName}`, JSON.stringify(errorScenes, null, 2));
+    (0, process_1.exit)();
+})();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
-/**
- * trantor console page saver, this script will open all page of the given console url and open the page, click the 'save' button and re-save the page.
- * 1. create a cluster of workers
- * 2. open the console page
- * 3. get all the scene page through api call
- * 4. add all the scene page url to the queue
- * 5. workers will pick the scene page from the queue and open the page and save the page
- * 6. workers will open the page and wait for the page to load. as soon as the save button is enabled, click the save button and re-save the page.
- */
 import puppeteer, { Browser } from "puppeteer";
 import bluebird from "bluebird";
 import axios from "axios";
 import dotenv from "dotenv";
 import { exit } from "process";
+import fs from "fs";
 
 dotenv.config();
 
@@ -37,14 +29,10 @@ if (process.env.TEAM_ID === undefined) {
   console.error("Please provide the team id");
   process.exit(1);
 }
-if (process.env.BRANCH_ID === undefined) {
-  console.error("Please provide the branch id");
-  process.exit(1);
-}
-if (process.env.PORTAL_ID === undefined) {
-  console.error("Please provide the portal id");
-  process.exit(1);
-}
+// if (process.env.BRANCH_ID === undefined) {
+//   console.error("Please provide the branch id");
+//   process.exit(1);
+// }
 
 // default 4
 const parallel = args.includes("--parallel")
@@ -56,6 +44,18 @@ if (!url) {
   process.exit(1);
 }
 
+const from = args.includes("--from")
+  ? args[args.indexOf("--from") + 1]
+  : "menu";
+
+const timeoutPerScene = args.includes("--timeout")
+  ? Number(args[args.indexOf("--timeout") + 1])
+  : 10 * 1000;
+
+const headless = args.includes("--headless")
+  ? args[args.indexOf("--headless") + 1] === "true"
+  : true;
+
 const request = axios.create({
   baseURL: url,
   headers: {
@@ -66,39 +66,34 @@ const request = axios.create({
   },
 });
 
+type ModuleMeta = {
+  id: string;
+  key: string;
+  name: string;
+  nativeModule: boolean;
+  teamId: string;
+  teamCode: string;
+};
+
+type ResourceMeta = {
+  key: string;
+  label: string;
+  parentKey: string;
+  type: string;
+  children?: ResourceMeta[];
+};
+
 // shared scene page queue
 type SceneMeta = {
   key: string;
   appId: string;
   teamId: string;
-  branchId: string;
+  // branchId: string;
 };
+
 const queue: SceneMeta[] = [];
-(async () => {
-  // get all the scene page through api call
-  const menu = await getMenu();
-  console.log("menu: ", menu.length);
-  pushAllScenesFromMenu(menu);
 
-  await replaceSceneKey(queue);
-
-  console.log("all scenes: ", queue.length);
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    defaultViewport: null,
-    args: ["--start-maximized"],
-  });
-
-  await bluebird.map(
-    queue,
-    (scene, index) => processQueue(scene, index, browser),
-    {
-      concurrency: parallel,
-    }
-  );
-  exit();
-})();
+const errorScenes: SceneMeta[] = [];
 
 type MenuItem = {
   routeType: "None" | "Scene";
@@ -127,21 +122,94 @@ async function getMenu() {
   return menu.data.data;
 }
 
-function pushAllScenesFromMenu(menu: MenuItem[]) {
-  for (const item of menu) {
-    if (item.routeType === "Scene") {
-      queue.push({
-        teamId: process.env.TEAM_ID!,
-        // TODO wrong appId
-        appId: undefined as any,
-        branchId: process.env.BRANCH_ID!,
-        key: item.routeConfig.sceneKey,
+/**
+ * 从菜单中获取所有的场景
+ * @param menu
+ */
+async function pushAllScenesFromMenu() {
+  const pushAllScenesFromMenuInternal = (menu: MenuItem[]) => {
+    for (const item of menu) {
+      if (item.routeType === "Scene") {
+        queue.push({
+          teamId: process.env.TEAM_ID!,
+          // TODO wrong appId
+          appId: undefined as any,
+          key: item.routeConfig.sceneKey,
+        });
+      }
+      if (item.children?.length) {
+        pushAllScenesFromMenuInternal(item.children);
+      }
+    }
+  };
+  const menu = await getMenu();
+  pushAllScenesFromMenuInternal(menu);
+  console.log("menu: ", menu.length);
+}
+
+/**
+ *
+ */
+async function getModules(teamId: string) {
+  const modules = await request.get<{ data: ModuleMeta[] }>(
+    `/api/trantor/console/module/query?type=Module`,
+    {
+      headers: {
+        "Trantor2-Team": process.env.TEAM_ID,
+        "Trantor2-Branch": process.env.BRANCH_ID,
+      },
+    }
+  );
+  return modules.data.data;
+}
+
+async function getScenesFromModule(teamId: string, moduleId: string) {
+  const { data: resourcesTree } = await request.get<{ data: ResourceMeta[] }>(
+    `/api/trantor/console/meta-data/resource-tree/folders?teamId=${teamId}&appId=${moduleId}&v2=true`,
+    {
+      headers: {
+        "Trantor2-App": moduleId,
+        "Trantor2-Team": process.env.TEAM_ID,
+        "Trantor2-Branch": process.env.BRANCH_ID,
+      },
+    }
+  );
+  // filter all the scenes
+  const scenes = [] as SceneMeta[];
+  const findScenes = (resource: ResourceMeta) => {
+    if (resource.type === "Scene") {
+      scenes.push({
+        key: resource.key,
+        appId: moduleId,
+        teamId: teamId,
+        // branchId: process.env.BRANCH_ID!,
       });
     }
-    if (item.children?.length) {
-      pushAllScenesFromMenu(item.children);
+    if (resource.children) {
+      resource.children.forEach(findScenes);
     }
-  }
+  };
+  resourcesTree.data.forEach(findScenes);
+  return scenes;
+}
+
+/**
+ * 从模块中获取所有的场景
+ * @param modules
+ */
+async function pushAllScenesFromModules() {
+  const modules = await getModules(process.env.TEAM_ID!);
+  const scenes = await Promise.all(
+    modules.map((module) =>
+      getScenesFromModule(process.env.TEAM_ID!, module.id)
+    )
+  );
+  scenes.forEach((scene) => queue.push(...scene));
+}
+
+async function pushAllScenesFromModule(moduleId: string) {
+  const scenes = await getScenesFromModule(process.env.TEAM_ID!, moduleId);
+  queue.push(...scenes);
 }
 
 async function replaceSceneKey(scenes: SceneMeta[]) {
@@ -176,7 +244,7 @@ async function processQueue(
   index: number,
   browser: Browser
 ) {
-  const retries = 3;
+  const retries = 2;
   for (let i = 0; i < retries; i++) {
     const page = await browser.newPage();
     // set cookie
@@ -187,25 +255,27 @@ async function processQueue(
     });
 
     // validate the sceneMeta
-    if (!sceneMeta.appId || !sceneMeta.branchId || !sceneMeta.key) {
+    if (!sceneMeta.appId || !sceneMeta.key) {
       console.error("invalid sceneMeta", sceneMeta);
       return;
     }
 
-    const sceneUrl = `${url}/team/${sceneMeta.teamId}/branch/${sceneMeta.branchId}/app/${sceneMeta.appId}/scene/${sceneMeta.key}`;
+    const sceneUrl = `${url}/team/${sceneMeta.teamId}${
+      process.env.BRANCH_ID ? `/branch/${process.env.BRANCH_ID}` : ""
+    }/app/${sceneMeta.appId}/scene/${sceneMeta.key}`;
     console.log("opening scene", sceneUrl);
     await page.goto(sceneUrl);
     try {
-      // 30s timeout, if the page is not loaded in 30s, it will throw error
-      const timeout = 10 * 1000;
       // wait for id=scene-save-button to be enabled
 
-      await page.waitForSelector("#scene-save-button:enabled", { timeout });
+      await page.waitForSelector("#scene-save-button:enabled", {
+        timeout: timeoutPerScene,
+      });
       // click save button
       await page.click("#scene-save-button");
       // wait for button to be not .ant-btn-loading
       await page.waitForSelector("#scene-unlock-button", {
-        timeout,
+        timeout: timeoutPerScene,
       });
       console.log("scene successfully saved", sceneUrl);
       console.log("scenes index: ", index);
@@ -218,8 +288,8 @@ async function processQueue(
         {
           headers: {
             "Trantor2-App": sceneMeta.appId,
-            "Trantor2-Team": sceneMeta.teamId,
-            "Trantor2-Branch": sceneMeta.branchId,
+            "Trantor2-Team": process.env.TEAM_ID,
+            "Trantor2-Branch": process.env.BRANCH_ID,
           },
         }
       );
@@ -227,8 +297,81 @@ async function processQueue(
       break;
     } catch (e) {
       console.error(e, sceneMeta.key, sceneMeta);
+      if (retries === i + 1) {
+        errorScenes.push(sceneMeta);
+      }
       await page.close();
     }
     console.log("retrying scene", sceneMeta.key);
   }
 }
+
+(async function main() {
+  // get all the scene page through api call
+  if (from === "modules") {
+    await pushAllScenesFromModules();
+  } else if (from === "module") {
+    if (process.env.APP_ID === undefined) {
+      console.error("Please provide the portal id");
+      process.exit(1);
+    }
+    await pushAllScenesFromModule(process.env.APP_ID);
+  } else {
+    if (process.env.PORTAL_ID === undefined) {
+      console.error("Please provide the portal id");
+      process.exit(1);
+    }
+    await pushAllScenesFromMenu();
+    await replaceSceneKey(queue);
+  }
+
+  console.log("all scenes: ", queue.length);
+
+  const browser = await puppeteer.launch({
+    headless,
+    defaultViewport: null,
+    args: ["--start-maximized"],
+  });
+
+  await bluebird.map(
+    queue,
+    (scene, index) => processQueue(scene, index, browser),
+    {
+      concurrency: parallel,
+    }
+  );
+  console.log("error scenes", errorScenes);
+  console.log("error scenes count", errorScenes.length);
+  await browser.close();
+
+  if (errorScenes.length === 0) {
+    console.log("All scenes are successfully saved");
+    exit();
+  }
+  // write error scenes to log file
+  const currentDate = new Date();
+  const host = process.env.HOST || "localhost";
+  const subdomain = host.split(".")[0];
+  const name = subdomain.split("://")[1];
+  const teamId = process.env.TEAM_ID || "unknown";
+  const appId = process.env.APP_ID || "unknown";
+  const portalId = process.env.PORTAL_ID || "unknown";
+  let fileName = `${name}-${teamId}`;
+  if (from === "menu") {
+    fileName += `-portal${portalId}`;
+  } else if (from === "module") {
+    fileName += `-module${appId}`;
+  } else {
+  }
+  fileName += `-${currentDate.toISOString()}.log`;
+  const logDirectory = "logs";
+  if (!fs.existsSync(logDirectory)) {
+    fs.mkdirSync(logDirectory);
+  }
+  fs.writeFileSync(
+    `${logDirectory}/${fileName}`,
+    JSON.stringify(errorScenes, null, 2)
+  );
+
+  exit();
+})();
